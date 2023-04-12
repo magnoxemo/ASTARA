35b5ce04cdb5823b3a5a83471e51da34fdd9206b
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-04-12 17:42:58 +0600

    Update reactor_MODEL.py
    not done  yet. have to write temperature function for fluid and moderator lumps

--- src/reactor_MODEL.py
+++ src/reactor_MODEL.py
@@ -25,6 +25,7 @@ from coding prespective:
     3.Reactor constants will be initialized via the OBJECT's constructor 
     4.Reactor and Reactor controller ---(in that case control rod driving system)
       will be two different objects but there will be a friend function for calculations and data interchange 
+
 from model prespective:
     1.The whole reactor will be considered as a uniform fuel rod 
     2.The core will be divided in different parts (15+2 lumps )
@@ -36,72 +37,107 @@ from model prespective:
 
 class Reactor ():
 
-    
+
     def __init__(self,Precursor_concentration:float,Area:float,
                  Coolant_heat_capacity:float,Fuel_heat_capacity:float,
                  Fission_power_fraction:float,Average_heat_transfer_factor:float
                  ,Coolant_mas_flow_rate:float,Coolant_mass_in_twofluid_nodes:float,
-                 Clod_leg_water_mass:float,Number_of_nodes:int,total_fuel_mass:float,
+                 Clod_leg_water_mass:float,fuel_nodal_mass:float,
                  Hot_leg_water_mass:float,Lower_plenum_water_mass:float,
                  Coolant_node_mass:float,upper_plenum_water_mass:float,
                  Reactor_core_power:float,Cold_leg_temp:float,
                  hot_leg_tem:float,Fluid_temp_in_lower_plenum:float,
                  Fluid_temp_in_upper_plenum:float,Fuel_temp_in_each_node:list,
                  Moderatro_temp_in_nodes:list,Steam_gen_inlet_temp:float,
-                 Reactivity_coefficient_coolant:float,Reactivity_coefficient_fuel:float,
-                 Total_delayed_neutron_group_fraction:float,
+                 Reactivity_coefficient_coolant:float,Reactivity_coefficient_fuel:float,beta:list,
                  neutron_group_const:list,neutron_generation_time:float,
-                 Total_reactivity:float,external_reactivity:float,timeconst_coldLeg:float,
-                 timeconst_hotLeg:float,timeconst_moderatorNode:float,
-                 timeconst_lower_plenum:float,timeconst_upper_plenum:float) -> None:
+                 Total_reactivity:float,external_reactivity:float,
+                 TempF0_initial_cond:list,TempM0_initial_cond:list) -> None:
         
         """local variables with appropriate notations"""
         self.C=Precursor_concentration
         self.A=Area
-        self.Node_num=Number_of_nodes
+        self.h=Average_heat_transfer_factor
         self.C_pc=Coolant_heat_capacity
         self.C_pf=Fuel_heat_capacity
         self.F_r=Fission_power_fraction
         self.M=Coolant_mas_flow_rate
         self.M_c=Coolant_mass_in_twofluid_nodes
         self.M_cl=Clod_leg_water_mass
-        self.M_f=total_fuel_mass/self.Node_num
+        self.M_f=fuel_nodal_mass
         self.M_hl=Hot_leg_water_mass
         self.M_lp=Lower_plenum_water_mass
         self.M_mo=Coolant_node_mass
         self.M_up=upper_plenum_water_mass
         self.P=Reactor_core_power
         self.T_cl=Cold_leg_temp
-        self.T_fuel_node=np.array(Fuel_temp_in_each_node)
+        self.T_fuel_node1=Fuel_temp_in_each_node[0]
+        self.T_fuel_node2=Fuel_temp_in_each_node[1]
+        self.T_fuel_node3=Fuel_temp_in_each_node[2]
         self.T_hl=hot_leg_tem
         self.T_lp=Fluid_temp_in_lower_plenum
         self.T_up=Fluid_temp_in_upper_plenum
-        self.T_moderator_node=np.array(Moderatro_temp_in_nodes)
+        self.T_moderator_node1=Moderatro_temp_in_nodes[0]
+        self.T_moderator_node2=Moderatro_temp_in_nodes[1]
+        self.T_moderator_node3=Moderatro_temp_in_nodes[2]
+        self.T_moderator_node4=Moderatro_temp_in_nodes[3]
+        self.T_moderator_node5=Moderatro_temp_in_nodes[4]
+        self.T_moderator_node6=Moderatro_temp_in_nodes[5]
         self.T_po=Steam_gen_inlet_temp
         self.alpha_c=Reactivity_coefficient_coolant
         self.alpha_f=Reactivity_coefficient_fuel
-        self.Beta_t=Total_delayed_neutron_group_fraction
+        self.beta=np.array(beta)
+        self.Beta_t=np.sum(beta)
         self.lemda=np.array(neutron_group_const)
         self.T_neutron_g=len(self.lemda)
         self.NGT=neutron_generation_time
         self.Ro=Total_reactivity
         self.Ro_ex=external_reactivity
-        self.Tau_cl=timeconst_coldLeg
-        self.Tau_hl=timeconst_hotLeg
-        self.Tau_C=timeconst_moderatorNode
-        self.Tau_lp=timeconst_lower_plenum
-        self.Tau_up=timeconst_upper_plenum
+        self.Tf0=np.array(TempF0_initial_cond)
+        self.TM0=np.array(TempM0_initial_cond)
 
-
-        if len(self.T_fuel_node)==self.Node_num:
-
-            pass
-
-        else:
-
-            raise AttributeError('Fuel node temperature missing')
         
-        if len(self.T_moderator_node)==2*self.Node_num:
-            pass
-        else:
-            raise AttributeError('Moderator node temperature missing')
+
+    def constitutive_cal(self):
+        sum_mod1=self.T_moderator_node1+self.T_moderator_node2+self.T_moderator_node3
+        +self.T_moderator_node4+self.T_moderator_node5+self.T_moderator_node6
+
+        sum_f1=self.T_fuel_node1+self.T_fuel_node2+self.T_fuel_node3
+
+        sum_mod0=np.sum(self.TM0)
+        sum_f0=np.sum(self.Tf0)
+
+
+        self.Tau_cl=self.M_cl/self.M
+        self.Tau_hl=self.M_hl/self.M
+        self.Tau_C=self.M_c/(2*self.M)
+        self.Tau_lp=self.M_lp/self.M
+        self.Tau_up=self.M_up/self.M
+
+        self.LAMDA =self.Beta_t/np.sum(self.beta/self.lemda)
+        self.Ro=self.Ro_ex+self.alpha_c*(sum_mod1-sum_mod0)/6+self.alpha_f*(sum_f1-sum_f0)/3
+
+    def dT_lp(self):
+
+        Tlp=(self.T_cl-self.T_lp)/self.Tau_lp
+
+        return Tlp
+    
+    def dT_up(self):
+
+        Tup=(self.T_moderator_node6-self.T_up)/self.Tau_up
+
+        return Tup
+    
+    def dT_hl(self):
+
+        Thl=(self.T_up-self.T_hl)/self.Tau_hl
+
+        return Thl
+    
+
+    def dT_cl(self):
+
+        Tcl=(self.T_po-self.T_cl)/self.Tau_cl
+
+        return Tcl
