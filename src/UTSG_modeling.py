1e76d8e51c026f557b0977865e49cf189ef41f55
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-04-13 23:46:53 +0600

    Update UTSG_modeling.py

--- src/UTSG_modeling.py
+++ src/UTSG_modeling.py
@@ -30,19 +30,40 @@ This will be a  generalized model of PWR.This code is based on this desetation:
 class UTSG():
 
     def __init__(self,Number_of_Utube:int,Tube_D_outside:float,
-                 Tube_D_inside:float,Tube_height:float,UTSG_height:float,
-                 flow_area_in_tube:float,flow_area_in_downcomer:float,
-                 flow_area_in_riser:float,flow_area_in_drum:float,riser_height:float,
-                 Primary_water_flow_rate:float,Primary_water_volume:float,
-                 specific_heat_primary_water:float,inlet_temp_pwater:float,
-                 outlet_temp_pwater:float,P_avg_Pside:float,avg_density:float,
-                 outlet_steam_flow_rate:float,steam_pressure:float,
-                 steam_temp_at_Spressure:float,inlet_temp_feed_water:float,
-                 avg_density_secondary_subcooled_water:float,E_heat_transfer_area:float,
-                 FHTC_primary_water:float,FHTC_secondary_subcooled_water:float,FHTC_secondary_boiling_water:float,
-                 Metal_Tube_conductivity:float) -> None:           
+                Tube_D_inside:float,Tube_height:float,UTSG_height:float,
+                secondaryFlow_area_in_the_U_tube_region:float,
+                area_of_the_drum_water_section:float,pressure_drop_coefficient_in_the_recirculating_loop:float,
+                Steam_valve_coefficient:float,Specific_heat_capacity_of_the_metaltubes:float,
+                Specific_heat_capacity_of_primary_subcooled_region_fluid:float,
+                Avg_enthalpy_boiling_region:float,Saturated_enthal_water:float,latent_enthal_water:float,
+                Ex_enthal_boiling_region:float,water_level_inUTSG:float,
+                Metal_mass_in_metal_node1:float,Metal_mass_in_metal_node2:float,
+                Mass_water_in_the_the_primarynode1:float,Mass_water_in_the_the_primarynode2:float,
+                Mass_water_in_the_the_primarynode3:float,Mass_water_in_the_the_primarynode4:float,
+                Mass_of_water_in_t_h_e_inlet_plenum:float,Steam_generator_pressure:float,
+                Heat_transfer_area_from_the_Utubes_to_the_secondary_side_in_the_subcooled:list,
+                Heat_transfer_area_from_the_Utubes_to_the_secondary_side_in_boiling_region:float,
+                Heat_transfer_areas_from_the_primary_side_to_the_Utubes_in_node1:float,
+                Heat_transfer_areas_from_the_primary_side_to_the_Utubes_in_node2:float,Downcomer_temperature:float,
+                Drum_water_temperature:float,Metal_tube_temperature_node1:float,Metal_tube_temperature_node2:float,
+                Metal_tube_temperature_node3:float,Metal_tube_temperature_node4:float,
+                Primary_coolant_temperatures_node1:float,Primary_coolant_temperatures_node2:float,
+                Primary_coolant_temperatures_node3:float,Primary_coolant_temperatures_node4:float,
+                Coolant_temperature_in_the_inlet_plenum:float,Coolant_temperature_in_the_outlet_plenum:float,
+                Saturated_temperature_of_the_water_and_steam_in_the_UTSG:float,
+                Heat_transfer_coefficient_from_the_primary_to_metal:float,
+                Heat_transfer_coefficient_from_the_metal_to__subcooled:float,
+                Heat_transfer_coefficient_from_the_metal_to_boiling_regions:float,
+                Volume_of_the_drum_section:float, Specific_volume_of_the_saturated_water:float,
+                Specific_volume_of_the_saturated_steam:float,Volume_of_the_riser_region:float,
+                Steam_flow_rate:float,Constant_parameters_of_the_water_property_equations:list,
+                steamquality_leaving_the_boiling_region:float,
+                Average_density_of_the_fluid_in_the_boiling_region:float,
+                Density_of_the_saturated_steam:float,Density_of_the_fluid_riser_region:float) -> None:           
         
                  #inlet temp of primary water= reactor's hot leg temp 
                  #inlet_temp_feed water=exit temp of the condenser
+
+        
 
         pass
