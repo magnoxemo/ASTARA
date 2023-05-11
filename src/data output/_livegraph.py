f3710a8edb2dda70f083125391d4573cf27eea3c
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-05-11 20:13:41 +0600

    Add files via upload

---
+++ src/data output/_livegraph.py
@@ -0,0 +1,13 @@
+"""live graph of different parameters """
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
+def ani(i):
+    plt.cla()
+    plt.plot(x[:i],y[:i])
+
+ani = animation.FuncAnimation(plt.gcf(), ani,interval=1)
+
+plt.show()
+    
