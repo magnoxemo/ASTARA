8608d8245ed66c7aa38c66d6e0dee11533427bac
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-06-29 08:53:48 +0600

    Update pump.py

--- src/pump.py
+++ src/pump.py
@@ -5,19 +5,31 @@ import scipy as sp
 from Reactor_MODEL import Reactor
 from UTSG_MODEL import UTSG
 
-class pump():
 
-    def __init__(self,nominal_speed:float,
-                 actual_speed:float,friction_factor:float,
-                 primar_loop_pipe_length:float,
-                 effective_area_piping_side:float) -> None:
+class reactor_primary_coolant_pump():
 
-        self.density_water=1000
-        self.friction_const=friction_factor
-        self.gravitaion=9.8
-        self.nominal_pump_speed=nominal_speed
-        self.actual_pump_speed=actual_speed
-        self.L=primar_loop_pipe_length
-        self.A_eff=effective_area_piping_side
+    def __init__(self,area_of_piping:float,piping_length:float,friction_factor:float,
+                 #initial_contidions
+                 moment_of_innertia:float,norminal_pump_speed:float,actual_pump_speed:float,norminal_flow_rate:float,actual_flow_rate:float,
+                 power_delivered_to_shaft:float,torque_delivered_to_shaft:float,Total_hydrodynamic_head_loss:float,
+                 developed_pump_head:float,Torque_delivered_to_pump_shaft:float,Hydrodynamic_torque:float):
 
-        pass
+        self.A=area_of_piping
+        self.L=piping_length
+        self.k=friction_factor
+        self.g=9.8
+        self.water_density=1000
+
+
+        self.Np=actual_pump_speed
+        self.Ns=norminal_pump_speed
+        self.Qp=actual_flow_rate
+        self.Qs=norminal_flow_rate
+        self.Pd=power_delivered_to_shaft
+        self.Td=torque_delivered_to_shaft
+        self.Hd=developed_pump_head
+        self.Hl=Total_hydrodynamic_head_loss
+        self.I=moment_of_innertia
+        self.Td=torque_delivered_to_shaft
+        self.Th=Hydrodynamic_torque
+
