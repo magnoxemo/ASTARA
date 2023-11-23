42cb5e633c0fe50c10cef0a80c9f89919406aa8c
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-11-23 19:26:10 +0600

    Update CondenserModel.py

--- src/Python3 codes/CondenserModel.py
+++ src/Python3 codes/CondenserModel.py
@@ -3,8 +3,7 @@ from CoolProp.CoolProp import PropsSI
 
 
 class Condenser():
-    def __init__(self,steampressure:float,airpressure:float,Temp_coldwaterin:float,
-                 Temp_coldwaterout:float):
+    def __init__(self,steampressure:float,airpressure:float):
 
         self.volume=3
         self.UA=356.972e3
@@ -16,7 +15,7 @@ class Condenser():
 
         """ steam mass balance variables"""
         self.W_turbine=4
-        self.W_otherthanturbine=10
+        self.W_otherthanturbine=0
         self.W_condensate=4
         self.W_steamairout=0
 
@@ -44,7 +43,8 @@ class Condenser():
         self.Hs=PropsSI('H','T',self.T_steamin,'P',self.Ps,'water')
         self.Hcw=PropsSI('H','T',self.T_coldwater,'P',101325,'water')
         self.deltaT=(self.T_hotwater-self.T_coldwater)/np.log((self.T_steamin-self.T_coldwater)/(self.T_steamin-self.T_hotwater))
-        self.Wc=(self.UA*self.delthaT)/(self.Hs-self.Hcw)
+        print(self.deltaT)
+        self.Wc=(self.UA*self.deltaT)/(self.Hs-self.Hcw)
 
 
     def DW_steam(self):
@@ -78,13 +78,13 @@ class Condenser():
         dtdWwHw=self.W_coldwater*self.Hcw+self.W_bubblingoxygen*Enthalpy_oxygen-self.W_hotwater*PropsSI('H',self.T_steamout,'P',pressure,'water')
         return dtdWwHw
     def DT_hotwater(self):
-        dtdTh=self.UA*self.deltaT-self.W_coldwater*self.Cp*(self.T_hotwater-self.T_coldwater)
+        dtdTh=(self.UA*self.deltaT-self.W_coldwater*self.Cp*(self.T_hotwater-self.T_coldwater))/(self.W_coldwater*self.Cp)
         return dtdTh
-   
-    def integrator(self,function,argsforfunction:list,intitial_cond,time_step):
+
+    def integrator(self,function,argsforfunction:None,intitial_cond,time_step):
         l=len(argsforfunction)
 
-        if l==0:
+        if l==0 or argsforfunction== None:
             return function()*time_step+intitial_cond
         elif l==1:
             arg1=argsforfunction[0]
@@ -99,5 +99,37 @@ class Condenser():
             arg3=argsforfunction[2]
             return function(arg1,arg2,arg3)*time_step+intitial_cond  
         else:
+            raise   AttributeError("agrs in your differential function were not correct! Fix them")
+
+Condenser=Condenser(steampressure=10e4,airpressure=9e3)
+
+dt=0.01
+t=0
+i=0
+time=[]
+y=[]
+
+"""case studied: Sudden rain and temp drop  """
+while t<1000:
+    Condenser.Ps=Condenser.integrator(Condenser.DPs,[],intitial_cond=Condenser.Ps,time_step=dt)
+    Condenser.W_turbine=Condenser.integrator(Condenser.DW_steam,[],intitial_cond=Condenser.W_turbine,time_step=dt)
+    Condenser.T_hotwater=Condenser.integrator(Condenser.DT_hotwater,[],intitial_cond=Condenser.T_hotwater,time_step=dt)
+    
+    t=t+dt
+    i=i+1
+    if t>50 and t<800:
+        Condenser.T_coldwater=Condenser.T_coldwater*np.exp(-(t-50)/1000000)
+    elif t>800:
+        Condenser.T_coldwater=Condenser.T_coldwater*0.01*np.exp((t-800)/100000)
+    else:
+        Condenser.T_coldwater=290+60
+    if i%100==0:
+        print ("%.3e" %Condenser.Ps,' ', "%.3e"%Condenser.T_hotwater)
+        time.append(t)
+        y.append(Condenser.T_hotwater)
+
+import matplotlib.pyplot as plt
+plt.plot(time,y)
+plt.show()
             raise   AttributeError("agrs in your differential function were not correct! Fix them")
         
