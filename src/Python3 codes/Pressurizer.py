aa330ecc482e9210e8d8e0df2cae0f8c7bf412ca
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-12-29 00:48:35 +0600

    Update Pressurizer.py

--- src/Python3 codes/Pressurizer.py
+++ src/Python3 codes/Pressurizer.py
@@ -1,165 +1,111 @@
-import numpy as np
+import numpy as np 
 from CoolProp.CoolProp import PropsSI
 from scipy.interpolate import interp1d
 
 class Pressurizer():
+    def __init__(self,pressurizer_diameter:float,Pressure:float,Temp_coldleg:float):
 
-    def __init__(self,area:float,effective_length_of_pressurizer:float,water_level:float,pressure_at_pressurizer:float,
-                 Temperature:float):
-        
-        """ 
-        Wsp---> spray flow rate that will come from the any of the cold leg of any of the 4 steam generator 
-        Wsr---> surge flow rate that will come from the any of the hot leg of any of the 4 steam generator 
+        self.Pressure=Pressure
+        self.diameter=pressurizer_diameter
+        self.l=14.2524
+        self.l_w=8.5527
+        self.J_p=5.4027
+        self.hw_bar=9.7209e5
 
-        so when there is any annomalies we either control the Heater (Q), Wsp or The Wsr
-        """
-        
-        #design parameters
-        self.area=area
-        self.L=effective_length_of_pressurizer
-        self.Ppr=pressure_at_pressurizer
-        self.Temp=Temperature
-
-        #constants 
-
-        pressure=np.linspace(700,2e7,num=20)
-        pressureGrad=np.gradient(pressure)
-
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("V","P",i,'Q',0,'water'))
-
-        self.Kp1=interp1d(pressure,np.gradient(blank)/pressureGrad)
-        
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("V","P",i,'Q',1,'water'))
-
-        self.Kp2=interp1d(pressure,np.gradient(blank)/pressureGrad)
-        self.Kp3=interp1d(pressure,self.Kp2(pressure)-self.Kp1(pressure))
-        
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("D","P",i,'Q',1,'water'))
-
-        self.Kp4=interp1d(pressure,np.gradient(blank)/pressureGrad)
-        
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("D","P",i,'Q',0,'water'))
-
-        self.Kp5=interp1d(pressure,np.gradient(blank)/pressureGrad)
-
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("H","P",i,'Q',0,'water'))
-
-        self.Kp6=interp1d(pressure,np.gradient(blank)/pressureGrad)
-        
-        blank=[]
-
-        for i in pressure:
-            blank.append(PropsSI("H","P",i,'Q',1,'water'))
-
-        self.Kpg=interp1d(pressure,np.gradient(blank)/pressureGrad)
-        self.Kp7=interp1d(pressure,self.Kpg(pressure)-self.Kp6(pressure))
-
-        
-        """ const[0]=d meu_f/dpr
-            const[1]=d meu_g/dpr
-            const[2]=d meu_fg/dpr
-            const[3]=d rou_s/dpr
-            const[4]=d rou_w/dpr
-            const[5]=d h_f/dpr
-            const[6]=d h_fg/dpr 
-        """
-
-        self.hf=PropsSI("H","P",self.Ppr,"Q",0,"water")
-        self.hg=PropsSI("H","P",self.Ppr,"Q",1,"water")
-        self.rouw=PropsSI("D","P",self.Ppr,"Q",0,"water")
-        self.rous=PropsSI("D","P",self.Ppr,"Q",1,"water")
-
-        self.hfg=self.hg-self.hf
-
-        self.hsp=PropsSI("H","P",self.Ppr,'T',self.Temp,'water')
-
-        self.vf=PropsSI("V","P",self.Ppr,"Q",0,"water")
-        self.vg=PropsSI("V","P",self.Ppr,"Q",1,"water")
-        self.vfg=self.vg-self.vf
-
-        self.Wsp=0
-        self.Wsr=0
+        ''' m_spray and Q automatic variable and depends on the operation. I will left this on the user to denfine 
+            when to operate them.
+        '''
+        self.m_spray=0
         self.Q=0
+        self.area=self.diameter**2*np.pi/4
+        '''
+        -----------------------------------------------------------------------------------------
+                                where the index j = 1 to N represent coolant nodes in the following
+                                order, lower plenum, coolant node 1 and 2, upper plenum, hot-leg, inlet
+                                plenum, PCL 1 and 2 and outlet plenum, and cold-leg. Based on the surge 
+                                will be calculated
+        -----------------------------------------------------------------------------------------------
+        '''
+        self.V_times_nu=np.array([.5991,0.1814,0.1814,1.3164,0.2752,0.277,0.022,0.6022,0.2776,0.1927])
+
+        ''' Q--->  0  ----> represents water
+            Q--->  1  ----> represents steam
+        '''
+        self.rho_w=PropsSI("D",'P',self.Pressure,'Q',0,'water')
+        self.rho_s=PropsSI("D","P",self.Pressure,'Q',1,'water')
+        self.Nu_w=1/PropsSI("D",'P',self.Pressure,'Q',0,'water')
+        self.Nu_s=1/PropsSI("D",'P',self.Pressure,'Q',1,'water')
+        self.h_w=PropsSI(("H",'P',self.Pressure,'Q',0,'water'))
+        self.h_spr=PropsSI("H","T",Temp_coldleg,'Q',0,'water')
+
+        '''------------------partial derivative co-efficient----------'''
+        self.k1p=PropsSI('d(D)/d(T)|P','P',self.Pressure,'Q',0,'Water')
+        self.k2p=PropsSI('d(D)/d(T)|P','P',self.Pressure,'Q',1,'Water')
+        self.k3p=PropsSI('d(h)/d(T)|P','P',self.Pressure,'Q',1,'Water')
         
+        '''-------------calculation of partital derivative ----------'''
+        Pressure_=np.logspace(5,17,7000)
+        density_=np.array(PropsSI("D",'P',Pressure_,'Q',0,'water'))
+        nu_s=1/density_
+        delta_nu_s=np.diff(nu_s)
+        delta_pressure=np.diff(Pressure_)
+        self.k4p=interp1d(delta_pressure,delta_nu_s)
 
-        #variables
+        '''------------------------------------------------ '''
 
-        self.Lw=water_level
-        self.Vw=self.area*self.Lw
+    def DPp(self,DTemp_list:list,Temp_coldleg):
 
-        #calculating some constant parameters 
+        '''------------------surge calculation----------'''
+        if len(DTemp_list)!=14:
+            raise ValueError('You need all the temperature derivative in the core:')
+        else:
+            m_surge=np.sum(self.V_times_nu*np.array(DTemp_list))
 
-        self.C1=self.rouw/self.rous -1
-        self.C2=(self.area*(self.L-self.Lw))*(self.rouw/self.rous)*self.Kp5(self.Ppr) +self.Kp4(self.Ppr)*self.area*self.Lw
+        nu_s=1/PropsSI("D","P",self.Pressure,'Q',1,'water')
+        h_spr=PropsSI("H","T",Temp_coldleg,'Q',0,'water')
+        h_w=PropsSI(("H",'P',self.Pressure,'Q',0,'water'))
+        rho_w=PropsSI("D",'P',self.Pressure,'Q',0,'water')
+        rho_s=PropsSI("D","P",self.Pressure,'Q',1,'water')        
+
+        C1p=(PropsSI("D",'P',self.Pressure,'Q',0,'water')/PropsSI("D","P",self.Pressure,'Q',1,'water'))-1
         
+        k3p=PropsSI('d(h)/d(T)|P','P',self.Pressure,'Q',1,'Water')
+        k4p=self.k4p(self.Pressure) #interpolated function 
+        Vw=self.area*self.l_w
+        Vs=self.area*(self.l-self.l_w)
 
+        a=self.Q+m_surge*((self.Pressure*nu_s)/(self.J_p*self.C1p))
+        b=self.m_spray*(h_spr-h_w+(self.hw_bar/C1p)+(self.Pressure/(rho_w*self.J_p*C1p)))
+        c=Vw*rho_w*(k3p+(k4p*self.Pressure)/self.J_p)+(Vs*rho_s*k4p*self.Pressure-Vw)/self.J_p
+        d=(self.hw_bar+self.Pressure*nu_s/self.J_p)
+
+        dtdPp=(a+b)/(c+d)
+        return dtdPp
     
-    def DLw(self):
+    def Dlw(self,DTemp_list:list,Temp_coldleg):
+        rho_s=PropsSI("D","P",self.Pressure,'Q',1,'water')
 
-        dtdLw=(1/(self.area*self.Lw))*(self.area*(self.L-self.Lw)*self.Kp5-self.C2/self.C1)*self.dPr()+self.Wsp/self.C1+self.Wco/self.C2
-        self.Wco=(self.C2*self.DPr()-self.Wsr-self.Wsp)/self.C1
-        return dtdLw
+        '''------------------surge calculation----------'''
+        if len(DTemp_list)!=14:
+            raise ValueError('You need all the temperature derivative in the core:')
+        else:
+            m_surge=np.sum(self.V_times_nu*np.array(DTemp_list))
 
-    def DPr(self):
+        k1p=PropsSI('d(D)/d(T)|P','P',self.Pressure,'Q',0,'Water')
+        k2p=PropsSI('d(D)/d(T)|P','P',self.Pressure,'Q',1,'Water')
+        C1p=(PropsSI("D",'P',self.Pressure,'Q',0,'water')/PropsSI("D","P",self.Pressure,'Q',1,'water'))-1
+        C2p=self.area*(self.l-self.l_w)*(C1p+1)*k2p+self.area*self.l_w*k1p
 
-        numerator=self.Q+self.Wsr*(self.Ppr*self.vg/(self.C1)+self.hfg/self.C1)\
-        +self.Wsp*(self.hsp-self.hf+self.hfg/self.C1+self.Ppr*(self.vg+self.vfg)/(self.C1))
+        a=(self.area*(self.l-self.l_w)*k2p-C1p/C2p)*self.DPp(DTemp_list,Temp_coldleg)
+        b=(m_surge/C1p)+((C2p*self.DPp(DTemp_list,Temp_coldleg))-m_surge-self.m_spray)/(C1p)**2
+        dtdlw=(a+b)/rho_s*self.area
 
-        denominator=self.Vw*self.rouw*((self.Kp6(self.Ppr))+self.Kp2(self.Ppr)*self.Ppr)+\
-            (self.L-self.Lw)*self.area*self.rous*self.Kp2(self.Ppr)*self.Ppr-self.Vw +\
-            (self.C2/self.C1)*(self.hfg+self.Ppr*(self.vf+self.vfg))
-
-        dtdPr=numerator/denominator
-
-        return dtdPr
+        return dtdlw
     
-    def PropertiesUpdater(self):
+    def integrator(self,function,argsforfunction:list,intitial_cond,time_step):
+        l=len(argsforfunction)
 
-        self.hf=PropsSI("H","P",self.Ppr,"Q",0,"water")
-        self.hg=PropsSI("H","P",self.Ppr,"Q",1,"water")
-        self.rouw=PropsSI("D","P",self.Ppr,"Q",0,"water")
-        self.rous=PropsSI("D","P",self.Ppr,"Q",1,"water")
-
-        self.hfg=self.hg-self.hf
-
-        self.hsp=PropsSI("H","P",self.Ppr,'T',self.Temp,'water')
-
-        self.vf=PropsSI("V","P",self.Ppr,"Q",0,"water")
-        self.vg=PropsSI("V","P",self.Ppr,"Q",1,"water")
-
-        self.Wsp=0
-        self.Wsr=0
-        self.Vw=self.area*self.Lw
-
-        #calculating some constant parameters 
-
-        self.C1=self.rouw/self.rous -1
-        self.C2=(self.area*(self.L-self.Lw))*(self.rouw/self.rous)*self.Kp5(self.Ppr) +self.Kp4(self.Ppr)*self.area*self.Lw
-
-    def integrator(self,function,argsforfunction:None,intitial_cond,time_step):
-        
-        try:
-            a=np.array(argsforfunction)
-            l=len(a)
-        except:
-            pass
-
-        if argsforfunction==None:
+        if l==0:
             return function()*time_step+intitial_cond
         elif l==1:
             arg1=argsforfunction[0]
@@ -174,5 +120,7 @@ class Pressurizer():
             arg3=argsforfunction[2]
             return function(arg1,arg2,arg3)*time_step+intitial_cond  
         else:
-            raise   AttributeError("agrs in your differential function were not correct! Fix them")
-        
+            raise   AttributeError("agrs in your differential function were not correct! Fix them")  
+
+
+'''------------------------------------------------------------ Done & final ---------------------------------------------------- '''   
